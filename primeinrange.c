231119762b86e3cfb52e1306cf6cab537cafbff5
shikhashrma <35444781+shikhashrma@users.noreply.github.com>
2018-01-24 00:51:54 +0530

    Add files via upload

--- primeinrange.c
+++ primeinrange.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 void main()
 {
-	int i,f,n,c=0,l;
+	int i,f,n,c,l;
 	printf("enter the range");
 	scanf("%d %d",&i,&f);
 	for(l=i+1;l<f;l++)
-	{
+	{c=0;
 		for(n=2;n<l;n++)
 		{
 			if(l%n==0)
 			c++;
+	
 		}
-		if(c>0)
+		if(c==0)
 		printf("%d ",l);
 	}
 }
